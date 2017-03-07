b9d1921674fa89e9cf37bf30eab9fbed6b91d22c
Douglas Fernandes <contato@douglasfernandes.com.br>
2017-03-07 14:54:56 -0300

    Fix

--- app/src/main/java/app/watchapp/activity/MainActivity.java
+++ app/src/main/java/app/watchapp/activity/MainActivity.java
@@ -41,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+        App.getEventBus().register(this);
 
         toolbar.setTitle(getResources().getString(R.string.app_name));
         setSupportActionBar(toolbar);
@@ -49,22 +50,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        //EventBus.getDefault().register(this);
-        App.getEventBus().register(this);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        //EventBus.getDefault().unregister(this);
+    protected void onDestroy() {
+        super.onDestroy();
         App.getEventBus().unregister(this);
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
     }
 
     @Override
@@ -78,7 +66,6 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.actionRemoveAll) {
-
             new AlertDialog.Builder(this)
                 .setTitle(getResources().getString(R.string.action_reset_list))
                 .setMessage(getResources().getString(R.string.warn_reset_list))
